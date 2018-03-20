184607022a040996d38320df6bc52e183ff4253e
Marco Pegoraro <marco.pegoraro@gmail.com>
2018-03-20 10:52:16 +0100

    empty box

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -4,16 +4,7 @@
 */
 
 import React from 'react'
-import { Route, Link } from 'react-router-dom'
 import { Helmet } from 'react-helmet'
-
-// Pure components tree
-import Dashboard from 'containers/Dashboard'
-import Posts from 'containers/Posts'
-import PostDetails from 'containers/PostDetails'
-// Side effected tree
-import Users from 'containers/Users'
-import Estimate from 'containers/Estimate'
 
 // import logo from './logo.svg'
 import './App.css'
@@ -24,23 +15,7 @@ const App = () => (
             <html lang="en" />
             <title>[cra-ssr] server side rendering for create react app</title>
         </Helmet>
-        <header className="App-header">
-            {/* <img src={logo} className="App-logo" alt="logo" /> */}
-            <div>
-                <Link to="/" style={{ color: '#fff' }}>Home</Link>
-                {' | '}
-                <Link to="/p" style={{ color: '#fff' }}>Posts</Link>
-                {' | '}
-                <Link to="/u" style={{ color: '#fff' }}>Users</Link>
-                {' | '}
-                <Link to="/estimate" style={{ color: '#fff' }}>Estimate</Link>
-            </div>
-        </header>
-        <Route exact path="/" component={Dashboard} />
-        <Route exact path="/p" component={Posts} />
-        <Route path="/p/:postId" component={PostDetails} />
-        <Route path="/u*" component={Users} />
-        <Route path="/estimate/:projectId?" component={Estimate} />
+        Hello Victor!
     </div>
 )
 
--- src/listeners/location-listener.js
+++ src/listeners/location-listener.js
@@ -2,33 +2,25 @@
 import createHistoryRouter from 'lib/redux-history-router'
 import { LOCATION_CHANGE } from 'services/location-service'
 
-import {
-    loadInitialPosts,
-    loadCurrentPost,
-    loadPostAuthor,
-    loadPostComments,
-} from 'services/posts-service'
-
 const applyRoutes = createHistoryRouter([
-    {
-        path: '/p',
-        exact: true,
-        action: () => async dispatch => dispatch(loadInitialPosts()),
-    },
-    {
-        path: '/p/:postId/:subMenu(author|comments)?',
-        action: ({ postId, subMenu }) => async (dispatch, getState) => {
-            
-            const post = await dispatch(loadCurrentPost(postId))
+    // {
+    //     path: '/p',
+    //     exact: true,
+    //     action: () => async dispatch => dispatch(loadInitialPosts()),
+    // },
+    // {
+    //     path: '/p/:postId/:subMenu(author|comments)?',
+    //     action: ({ postId, subMenu }) => async (dispatch) => {
+    //         const post = await dispatch(loadCurrentPost(postId))
 
-            // load sub-route data
-            if (subMenu === 'author') {
-                dispatch(loadPostAuthor(post))
-            } else if (subMenu === 'comments') {
-                dispatch(loadPostComments(post))
-            }
-        },
-    },
+    //         // load sub-route data
+    //         if (subMenu === 'author') {
+    //             dispatch(loadPostAuthor(post))
+    //         } else if (subMenu === 'comments') {
+    //             dispatch(loadPostComments(post))
+    //         }
+    //     },
+    // },
 ])
 
 export default [{
